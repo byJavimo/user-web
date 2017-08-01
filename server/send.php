ea7afc67323b9b259b45f8d4547f08931c2c01e3
Javier Mora <javier.mora@kbc.be>
2017-08-01 15:31:55 +0200

    Adding php files to send forms

---
+++ server/send.php
@@ -0,0 +1,27 @@
+<?php
+  error_reporting(0);
+  $name = $_POST['recruiterName'];
+  $lastName=$_POST['recruiterLastName'];
+  $email= $_POST['recruiterEmail'];
+  $company = $_POST['recruiterCompany'];
+  $subject = $_POST['recruiterSubject'];
+  $description = $_POST['recruiterDescription']
+
+  $header = 'From: ' . $recruiterEmail . "\r\n";
+  $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+  $header .= "Mime-Version: 1.0 \r\n";
+  $header .= "Content-Type: text/plain";
+
+  $message = "Este mensaje fue enviado por " . $name . " \r\n";
+  $message .= "Su e-mail es: " . $email . " \r\n";
+  $message .= "Empresa: " . $company . " \r\n";
+  $message .="Comentario: " . $description " \r\n";
+  $message .= "Enviado el " . date('d/m/Y', time());
+
+  $targetEmail = 'javier.mora.alcazar@gmail.com';
+
+
+  mail($targetEmail, $subject, utf8_decode($message), $header);
+
+  echo 'Message successfully sent';
+?>
